c23b5df0a2f61d07a111e7331a582c9e0a546a00
Sebastian Koske <sebastian.koske@icloud.com>
2023-06-05 07:31:21 +0200

    add SftLilypond

--- Package.swift
+++ Package.swift
@@ -14,15 +14,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "SftMusicModel", path: "/Users/sebastiankoske/Development/Swift/MusicBox/packages/SftMusicModel"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SftLilypond",
-            dependencies: []),
-        .testTarget(
-            name: "SftLilypondTests",
-            dependencies: ["SftLilypond"]),
+            dependencies: ["SftMusicModel"]),
     ]
 )
